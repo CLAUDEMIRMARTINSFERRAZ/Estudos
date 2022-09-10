6c0ee7cbb05ffd5527438d4efcc3887bf437f8e5
CLAUDEMIR MARTINS FERRAZ <claudemirmferraz@gmail.com>
2022-09-10 10:27:22 -0300

    Simulador de Dado
    Codigo feito para estudos de programação

--- JogandoODado.py
+++ JogandoODado.py
@@ -1,1 +1,40 @@
-esse é meu primeiro codigo criado para teste.
+
+import random
+import PySimpleGUI as sg
+
+
+class SimuladorDeDado:
+    def __init__(self):
+        self.valor_mimimo = 1
+        self.valor_maximo = 6
+
+         #Layout
+       self.layout = [
+            [sg.Text('Jogar o Dado?')],
+            [sg.Button ('Sim'),sg.Button ('Não')]
+        ]
+        
+    def Iniciar(self):
+        #Criar uma janela
+        self.janela = sg.Window ('Simulador De Dado', Layout=self.layout)
+
+        #Ler os valores da Tela
+        self.eventos, self.valores = self.janela.Read()
+
+        #Fazer alguma coisa com esses valores
+            try:
+                if self.eventos =='Sim' or self.eventos == 's':
+                    self.GerarValorDoDado()
+                elif self.eventos == 'Não' or self.eventos == 'n':
+                    print ('Agradecemos sua participação!')
+                else:
+                    print ('Favor digite Sim ou Não !!!')
+            except:
+                print('Ocoreu um erro ao receber  sua resposta')
+
+    def GerarValorDoDado(self):
+        print(random.randint(self.valor_mimimo,self.valor_maximo))
+
+    
+simulador = SimuladorDeDado()
+simulador.Iniciar()
