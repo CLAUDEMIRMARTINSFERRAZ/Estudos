1251133117740d998bd3b6aa1600d628880a7dd3
CLAUDEMIR MARTINS FERRAZ <claudemirmferraz@gmail.com>
2022-09-15 17:49:20 -0300

    colocando tela no programa

--- Jogo_de_aventura.py
+++ Jogo_de_aventura.py
@@ -5,7 +5,7 @@ import PySimpleGUI as sg
 class JogoDeAventura:
     def __init__(self):
         self.pergunta1 = 'Você  nasceu no Norte ou no Sul? (n ou s)' #norte é Lado A  e Sul lado B
-        self.pergunta2 = 'Você prefere a espada ou o escudo? (sspada ou sscudo)' #Espada = lado Norte. Escudo la Sul
+        self.pergunta2 = 'Você prefere a espada ou o escudo? (espada ou escudo)' #Espada = lado Norte. Escudo la Sul
         self.pergunta3 = 'Qual sua especialidade? (tatico ou linha de frente)!' #Atague lado Norte, Devfesa lado Sul
         # Lado 2
         self.finaldaHistoria1 = 'Voce será u heroi n alinha de frente!'
@@ -18,10 +18,10 @@ class JogoDeAventura:
         layout = [
             [sg.Output(size=(55,10))],
             [sg.Input(size=(35,0),key='escolha')],
-            [sg.Button('Iniciar o Jogo'), sg.Button('RESPONDER')],
+            [sg.Button('Iniciar o Jogo'), sg.Button('Responder')],
         ]
         #criar a janela
-        Self.janela = sg.Window('JOGO DE AVENTURA, (A BATANHA', layout=layout)
+        Self.janela = sg.Window('JOGO DE AVENTURA, (A BATANHA)', layout=layout)
         while True:
 
             #Ler os dados coletados
